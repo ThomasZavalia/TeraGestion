5e9577311b3b20e2dafab05b2457f1a7e0e9d751
decademicorazon <zavaliathomas2@gmail.com>
2025-10-07 15:02:07 -0300

    Dni arreglado

--- Infraestructure/TeraDbContextFactory.cs
+++ Infraestructure/TeraDbContextFactory.cs
@@ -35,31 +35,49 @@ namespace Infrastructure
 
     public class TeraDbContextFactory : IDesignTimeDbContextFactory<TeraDbContext>
     {
+        /* public TeraDbContext CreateDbContext(string[] args)
+         {
+             var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Controllers")) 
+                 .Build();
+
+             var provider = configuration["Database:Provider"];
+             var connectionString = configuration["Database:ConnectionString"];
+
+             var optionsBuilder = new DbContextOptionsBuilder<TeraDbContext>();
+
+             switch (provider)
+             {
+                 case "Postgres":
+                     optionsBuilder.UseNpgsql(connectionString);
+                     break;
+                 case "MySQL":
+                     optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+                     break;
+                 case "SQLite":
+                     optionsBuilder.UseSqlite(connectionString);
+                     break;
+                 default:
+                     throw new Exception("Proveedor de base de datos no soportado.");
+             }
+
+             return new TeraDbContext(optionsBuilder.Options);
+         }*/
+
         public TeraDbContext CreateDbContext(string[] args)
         {
+            // Ruta al proyecto Controllers (donde está appsettings.json)
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../Controllers");
+
             var configuration = new ConfigurationBuilder()
-                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Controllers")) 
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false)
                 .Build();
 
-            var provider = configuration["Database:Provider"];
-            var connectionString = configuration["Database:ConnectionString"];
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
 
             var optionsBuilder = new DbContextOptionsBuilder<TeraDbContext>();
-
-            switch (provider)
-            {
-                case "Postgres":
-                    optionsBuilder.UseNpgsql(connectionString);
-                    break;
-                case "MySQL":
-                    optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-                    break;
-                case "SQLite":
-                    optionsBuilder.UseSqlite(connectionString);
-                    break;
-                default:
-                    throw new Exception("Proveedor de base de datos no soportado.");
-            }
+            optionsBuilder.UseNpgsql(connectionString);
 
             return new TeraDbContext(optionsBuilder.Options);
         }
